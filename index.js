4733522598b2cc96ea720cb7672ab2c679e605bb
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-04-03 19:28:33 +0100

    initial develop branch setup, basic basic app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>
+{
+    res.send(`Hello from YelpCamp, served from port ${port}!`)
+});
+
+app.listen(port, () =>
+{
+    console.log(`Serving on port ${port}`)
+});
