5f67821380429e6cab207ae0a72d2c74721790d5
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-04-03 20:29:57 +0100

    adds basic mongodb and mongoose wiring
    can now add an entry to the database by visiting the get route of makecampgrounds

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const path = require("path");
+
+// start mongoose
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+// hardcoded address for now, whilst i setup
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() =>
+    {
+        console.log(`MongoDB Connection Open :)`);
+    })
+    .catch((err) =>
+    {
+        console.log("Oh no! MongoDB Connection Error :(");
+        console.log(err);
+    });
+// end mongoose
+
 const app = express();
 const port = 3000;
-const path = require("path");
+
 
 app.set("view engine", "ejs");
 // __dirname is the absolute file location
@@ -12,7 +30,20 @@ app.get("/", (req, res) =>
     res.render("home");
 });
 
+// test route - this should be post but whatever
+app.get("/makecampground", async (req, res) =>
+{
+    const camp = new Campground({ title: "My Garden", description: "It's free!", price: 0.00 });
+    await camp.save();
+    res.send(camp);
+});
+
 app.listen(port, () =>
 {
     console.log(`Serving on port ${port}`)
 });
+
+// campgrounds have:
+// a name/title, a price, a description
+// a location (just a string at this stage)
+// 
