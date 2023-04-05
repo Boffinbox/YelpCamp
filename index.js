02ebdfa7f49bf07e9760d4c01e672212b78ba5f2
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-04-05 20:33:17 +0100

    add a post route for submitting new campgrounds
    also adds return links across the views that already exist
    these will be refactored into partials later on, but right now
    i just want to work

--- index.js
+++ index.js
@@ -25,23 +25,44 @@ app.set("view engine", "ejs");
 // __dirname is the absolute file location
 app.set("views", path.join(__dirname, "/views"));
 
+// post handling
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) =>
 {
     res.render("home");
 });
 
-// index route
+// show index route
 app.get("/campgrounds", async (req, res) =>
 {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
 });
 
+// show create form
+app.get("/campgrounds/new", async (req, res) =>
+{
+    res.render("campgrounds/new");
+});
+
+// actual create route - redirect to read page
+app.post("/campgrounds", async (req, res) =>
+{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// show read route
 app.get("/campgrounds/:id", async (req, res) =>
 {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });
 });
+
+
+
 
 app.listen(port, () =>
 {
