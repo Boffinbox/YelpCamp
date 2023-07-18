9f0be16fc945886fe7c9f2b86c8a896177e4ae6f
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-07-18 17:34:12 +0100

    add virtual thumbnail property to allow cloudinary transformations

--- models/campground.js
+++ models/campground.js
@@ -2,17 +2,26 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/dejzcp0js/image/upload/v1689607713/YelpCamp/xskfqxlb8df513ml3mgp.jpg
+
+const ImageSchema = new Schema(
+    {
+        url: String,
+        filename: String
+    }
+)
+
+ImageSchema.virtual("thumbnail").get(function ()
+{
+    return this.url.replace("/upload", "/upload/w_200");
+})
+
 const campgroundSchema = new Schema({
     title:
     {
         type: String, required: true
     },
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
