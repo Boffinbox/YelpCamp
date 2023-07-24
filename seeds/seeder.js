6d8d6c2434c61d605ee4714f44b1bc8a56253104
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-07-24 18:55:12 +0100

    fix seeder file now adds 3 campsites per call

--- seeds/seeder.js
+++ seeds/seeder.js
@@ -1,6 +1,11 @@
 // antime we need to seed/reset the db
 // i will run this
 
+if (process.env.NODE_ENV !== "production")
+{
+    require("dotenv").config();
+}
+
 // start mongoose
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
@@ -22,22 +27,34 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 const countries = require("./countries");
 const { descriptors, places } = require("./campNames");
 
+const geo = require("../helpers/geometry");
+
 const seedDB = async () =>
 {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++)
+    // break glass in event of total failure
+    // //await Campground.deleteMany({});
+    for (let i = 0; i < 3; i++)
     {
         // math floor because of zero index array
         const price = Math.ceil(Math.random() * 25) + 10;
         const c = new Campground({
             title: `${randomFromArray(descriptors)} ${randomFromArray(places)}`,
-            location: `${randomFromArray(countries)}`,
-            image: "https://source.unsplash.com/collection/483251",
+            price: price,
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, soluta! Eos maxime dolorum aut perspiciatis veniam ratione vitae eum. Dignissimos tenetur culpa, autem fugiat debitis alias eos veritatis molestiae animi!",
-            price: price
+            images: [
+                {
+                    url: "https://res.cloudinary.com/dejzcp0js/image/upload/v1689778828/YelpCamp/afoixxczbteeubvz6myn.jpg",
+                    filename: 'YelpCamp/afoixxczbteeubvz6myn'
+                }
+            ],
+            reviews: [],
+            author: "6491f01ad5a77a2dd1c8ab80"
         });
+        c.location = `${randomFromArray(countries)}`;
+        const geoData = await geo.getGeoData(c.location);
+        c.geometry = geo.getGeometry(geoData);
         await c.save();
-        await console.log(`camp site ${i + 1} saved`);
+        await console.log(`camp site ${i + 1} "saved"`);
     }
     // once done seeding, close and finish up
     mongoose.connection.close();
