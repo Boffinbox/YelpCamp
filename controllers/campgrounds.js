69888cf21bb8fcc2e9aa241de8805559cd41f53b
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-07-24 17:23:05 +0100

    refactor geo functions out to geometry helper

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,9 +1,6 @@
 const Campground = require("../models/campground");
 const ExpressError = require("../helpers/expresserror")
-
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+const geo = require("../helpers/geometry")
 
 const { cloudinary } = require("../cloudinary");
 
@@ -21,8 +18,8 @@ module.exports.renderNewForm = (req, res) =>
 module.exports.createCampground = async (req, res) =>
 {
     const campground = new Campground(req.body.campground);
-    const geoData = await getGeoData(campground.location)
-    campground.geometry = getGeometry(geoData);
+    const geoData = await geo.getGeoData(campground.location)
+    campground.geometry = geo.getGeometry(geoData);
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = await req.user._id;
     await campground.save();
@@ -79,8 +76,8 @@ module.exports.updateCampground = async (req, res) =>
     Object.assign(campground, req.body.campground);
     if (campground.location !== origLocation)
     {
-        const geoData = await getGeoData(campground.location);
-        campground.geometry = getGeometry(geoData);
+        const geoData = await geo.getGeoData(campground.location);
+        campground.geometry = geo.getGeometry(geoData);
     }
     // then push the images onto the campground
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
@@ -120,35 +117,4 @@ module.exports.destroyCampground = async (req, res) =>
     }
     req.flash("success", "Campground deleted successfully")
     res.redirect("/campgrounds");
-}
-
-// location should be a simple string,
-// such as "spain" or "kraków, poland"
-async function getGeoData(location)
-{
-    const geoData = await geocoder.forwardGeocode(
-        {
-            query: location,
-            limit: 1
-        }
-    ).send();
-    console.log("Mapbox Forward Geocode has been called once.");
-    return geoData
-}
-
-// returns a geometry, and if geoData didn't
-// get a result, returns a 0,0 point
-function getGeometry(geoData)
-{
-    if (geoData.body.features.length === 0)
-    {
-        return {
-            type: "Point",
-            coordinates: [0.000000, 0.000000]
-        }
-    }
-    else
-    {
-        return geoData.body.features[0].geometry;
-    }
 }
---
+++ helpers/geometry.js
@@ -0,0 +1,41 @@
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+
+// location should be a simple string,
+// such as "spain" or "kraków, poland"
+async function getGeoData(location)
+{
+    const geoData = await geocoder.forwardGeocode(
+        {
+            query: location,
+            limit: 1
+        }
+    ).send();
+    console.log("Mapbox Forward Geocode has been called once.");
+    return geoData
+}
+
+// returns a geometry, and if geoData didn't
+// get a result, returns a 0,0 point
+function getGeometry(geoData)
+{
+    if (geoData.body.features.length === 0)
+    {
+        console.log("No valid point was found. Setting to [0°E, 0°N]...");
+        return {
+            type: "Point",
+            coordinates: [0.000000, 0.000000]
+        }
+    }
+    else
+    {
+        return geoData.body.features[0].geometry;
+    }
+}
+
+module.exports =
+{
+    getGeoData,
+    getGeometry
+};
