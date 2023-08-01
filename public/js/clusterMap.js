409105d6703bda1323c32e3bc2129f9df4801cbb
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-08-01 20:20:33 +0100

    update maps now use separate stylesheet

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -3,7 +3,7 @@
 // https://account.mapbox.com
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'clustermap',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/outdoors-v12',
     center: [15, 52],
