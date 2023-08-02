f48c25cb69bb18ab5a0e6b619b7a62013e70ae83
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-08-02 20:07:57 +0100

    add basic content security policy, and other things, using helmet

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 const ExpressError = require("./helpers/expresserror");
 
@@ -105,6 +106,55 @@ const sessionConfig =
 }
 app.use(session(sessionConfig));
 app.use(flash());
+
+// big block of security stuff
+// basically whitelisting external resources
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net"
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/"
+];
+const fontSrcUrls = [];
+
+const csp = {
+    directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ['self', "blob:"],
+        objectSrc: [],
+        imgSrc: [
+            "'self'",
+            "blob:",
+            "data:",
+            `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+        // remove this one in production?
+        upgradeInsecureRequests: null
+    },
+}
+
+app.use(helmet({ contentSecurityPolicy: csp }));
 
 app.use(passport.initialize());
 app.use(passport.session());
