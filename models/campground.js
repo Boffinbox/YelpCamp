c8df4e62cc9d9468134f8f67b52dcd13b5db6213
Adam Gwenter <boffinbox@hotmail.co.uk>
2023-08-05 18:40:07 +0100

    fix image thumbnails now correct size for index map markers

--- models/campground.js
+++ models/campground.js
@@ -57,7 +57,7 @@ campgroundSchema.virtual("properties.popUpMarkup").get(function ()
     <div style="text-align: center">
     <h5>${this.title}</h5>
     <p>${this.description.substring(0, 72)}...</p>
-    <p><img src="${this.images[0].thumbnail}"></p>
+    <p><img src="${this.images[0].thumbnail}" style="width: 200px; height: 200px></p>
     <div><a href="/campgrounds/${this._id}">View Campground</div>
     </div>`;
 });
